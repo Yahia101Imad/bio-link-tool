0b09ced01d960cb565adb874b79f59a741eecfdd
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-03-16 08:03:30 +0100

    feat: add token to localstorage

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
+// CRESTING BASE URL WITH AXIOS
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+// CONNECTING API WITH PAGES
 export const registerUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
 
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return req;
-// });
+// SAVE TOKEN IN LOCALSTORAGE AFTER POSTING REQUEST
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
 
 export default API;
