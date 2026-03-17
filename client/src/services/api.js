46c86f095033487d5f98c98136c0f6a2a4a55176
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-03-17 18:22:25 +0100

    fix: archetecture problem

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import AppError from '../../../server/utils/AppError'
+import AppError from '../utils/AppError'
 
 // CRESTING BASE URL WITH AXIOS
 const API = axios.create({
